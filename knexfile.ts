4a9908f81f317e8d808b2722821351e07c049812
novalCandra <dinatanovel5@gmail.com>
2026-09-13 12:36:21 +0700

    feat : 🛠️ repair configuration (knex)

--- knexfile.ts
+++ knexfile.ts
@@ -1,48 +1,16 @@
-module.exports = {
-  development: {
+import type { Knex } from "knex";
+const config: Knex.Config<any> = {
     client: "mysql2",
     connection: {
       host: "127.0.0.1",
       port: 3306,
       user: "root",
-      password: "",
+      password: "password",
       database: "database-viraoliv"
     },
     migrations: {
       tableName: "knex_migrations"
     }
-  },
-
-  staging: {
-    client: "mysql2",
-    connection: {
-      host: "127.0.0.1",
-      port: 3306,
-      user: "root",
-      password: "",
-      database: "database-viraoliv"
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "mysql2",
-    connection: {
-      host: "127.0.0.1",
-      port: 3306,
-      user: "root",
-      password: "",
-      database: "database-viraoliv"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
 };
 
+export default config
