f84fec5a37a9761c2005f2607c4b82c9c420e421
novalCandra <dinatanovel5@gmail.com>
2026-09-19 10:56:56 +0700

    repair : 🛠️ Repair Server

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 
 import express from "express";
-import cors from "cors"
-import { AuthRouter } from "./src/router/AuthRouter.router.js";
-import { google } from "googleapis"
-import { Request, Response } from "express";
-import { serviceGoogle } from "./src/service/Auth.service.js";
+import cors from "cors";
+import { AuthRouter } from "./src/router/AuthRouter.route.js";
 import { UserRouter } from "./src/router/User.route.js";
 import { CategoriesRouter } from "./src/router/categorie.route.js";
 import { taskRouter } from "./src/router/task.route.js";
+import { NotedRouter } from "./src/router/noted.router.route.js";
 require('dotenv').config()
 const app = express()
 const port = 3001
@@ -25,65 +23,7 @@ app.use("/api", AuthRouter)
 app.use("/api", UserRouter)
 app.use("/api", CategoriesRouter)
 app.use("/api", taskRouter)
-
-// QAUTH 3.0
-const ouathClient3 = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    'http://localhost:3001/auth/google/callback'
-)
-
-const scopes = [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile'
-]
-
-const url = ouathClient3.generateAuthUrl({
-    access_type: "online",
-    include_granted_scopes: true,
-    scope: scopes
-})
-
-app.get("/auth/google", (req, res) => {
-    res.redirect(url)
-})
-
-app.get("/auth/google/callback", async (req: Request, res: Response) => {
-    try {
-        const { code } = req.query;
-        const { tokens } = await ouathClient3.getToken(code);
-        ouathClient3.setCredentials(tokens)
-        const oauth2 = google.oauth2({
-            auth: ouathClient3,
-            version: "v2"
-        });
-
-        const { data } = await oauth2.userinfo.get();
-        if (!data) {
-            return res.status(403).json({
-                status: false,
-                message: "gagal login menggunakan google"
-            })
-        }
-
-        await serviceGoogle({
-            name: data.name,
-            email: data.email
-        })
-
-        return res.status(201).json({
-            status: true,
-            message: "success login google",
-            data: data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status: false,
-            message: "server error",
-            messageError: error
-        })
-    }
-})
+app.use("/api", NotedRouter)
 
 app.listen(port, () => {
     return console.log(`BACK END BERJALAN FOR PORT : ${port}`)
